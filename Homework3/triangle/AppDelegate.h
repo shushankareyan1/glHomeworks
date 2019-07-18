b19ae67415da7954e9b907f2583273577a8eaf83
shushankareyan1 <shushan.kareyan@picsart.com>
2019-07-18 10:56:53 +0400

    folder fix

---
+++ Homework3/triangle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  triangle
+//
+//  Created by TestUser on 8/4/15.
+//  Copyright (c) 2015 TestUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework3/triangle/Shaders/Shader.fsh
@@ -0,0 +1,14 @@
+//
+//  Shader.fsh
+//  triangle
+//
+//  Created by TestUser on 8/4/15.
+//  Copyright (c) 2015 TestUser. All rights reserved.
+//
+
+varying lowp vec4 colorVarying;
+
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 0.0);
+}
